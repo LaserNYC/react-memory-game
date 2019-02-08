f076a224f29d819d8875d67039e13d41fcb1ed61
David Rahn <dave@Davids-Mac-mini.fios-router.home>
2019-02-08 15:30:52 -0500

    Updates

---
+++ precache-manifest.d85e3ff3616b4ec4e287eee15dad85b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "828dbb2244d084e55b1595d52f64f490",
+    "url": "/react-memory-game/static/media/icomoon.828dbb22.woff"
+  },
+  {
+    "revision": "efc8a0bed168c5c6af91",
+    "url": "/react-memory-game/static/js/runtime~main.efc8a0be.js"
+  },
+  {
+    "revision": "708622b6fe9f07bf9c4f",
+    "url": "/react-memory-game/static/js/main.708622b6.chunk.js"
+  },
+  {
+    "revision": "b41e33c65c30a9b84799",
+    "url": "/react-memory-game/static/js/1.b41e33c6.chunk.js"
+  },
+  {
+    "revision": "708622b6fe9f07bf9c4f",
+    "url": "/react-memory-game/static/css/main.185abfec.chunk.css"
+  },
+  {
+    "revision": "9b137924fc8e9447b9d80fb32fd235db",
+    "url": "/react-memory-game/index.html"
+  }
+];
